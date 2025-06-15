44314c53d93c2b07fc17e4d3bb3af97e6fc5e57a
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-16 01:14:06 +0330

    add DomainEvent interface for event sourcing with aggregate ID and timestamp

---
+++ src/main/java/dev/amirgol/springcqrs/domain/event/DomainEvent.java
@@ -0,0 +1,11 @@
+package dev.amirgol.springcqrs.domain.event;
+
+import dev.amirgol.springcqrs.domain.vo.CustomerId;
+import dev.amirgol.springcqrs.domain.vo.FullName;
+
+import java.time.Instant;
+
+public interface DomainEvent {
+    CustomerId getAggregateId();
+    Instant getOccurredOn();
+}
