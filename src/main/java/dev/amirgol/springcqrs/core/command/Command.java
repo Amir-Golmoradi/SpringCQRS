5b9a2e706d304ea25fb41b75236a465924c9e45e
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-16 01:13:22 +0330

    add Command and Query marker interfaces for command-query separation

---
+++ src/main/java/dev/amirgol/springcqrs/core/command/Command.java
@@ -0,0 +1,11 @@
+package dev.amirgol.springcqrs.core.command;
+
+/**
+ * Marker interface for commands.
+ * Created by: Amir-Golmoradi
+ * Created on: 2025-06-14 18:27:03
+ */
+
+public interface Command<R> {
+
+}
---
+++ src/main/java/dev/amirgol/springcqrs/core/query/Query.java
@@ -0,0 +1,9 @@
+package dev.amirgol.springcqrs.core.query;
+
+/**
+ * Marker interface for queries.
+ * Created by: Amir-Golmoradi
+ * Created on: 2025-06-14 18:27:03
+ */
+public interface Query<R> {
+}
