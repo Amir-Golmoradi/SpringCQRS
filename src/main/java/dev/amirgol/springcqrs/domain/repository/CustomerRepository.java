55a68b2ce3222cf9545f261aa18689190c02e9ef
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-16 01:13:02 +0330

    add CustomerRepository interface for customer data operations

---
+++ src/main/java/dev/amirgol/springcqrs/domain/repository/CustomerRepository.java
@@ -0,0 +1,22 @@
+package dev.amirgol.springcqrs.domain.repository;
+
+import dev.amirgol.springcqrs.domain.model.Customer;
+import dev.amirgol.springcqrs.domain.vo.CustomerId;
+import dev.amirgol.springcqrs.domain.vo.Email;
+import dev.amirgol.springcqrs.domain.vo.FullName;
+
+import java.util.Optional;
+
+public interface CustomerRepository {
+    Optional<Customer> findById(CustomerId id);
+
+    Optional<Customer> findByName(FullName fullName);
+
+    Optional<Customer> findByEmail(Email email);
+
+    void register(Customer customer);
+
+    void update(Customer customer);
+
+    void deleteByEmail(Email email);
+}
