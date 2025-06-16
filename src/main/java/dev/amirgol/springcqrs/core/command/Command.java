a101d15f1ed3fb1503f0a7d1762d1805c7cd2e30
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-17 01:27:46 +0330

    docs(Command): Enhance documentation for marker interface and clarify purpose

--- src/main/java/dev/amirgol/springcqrs/core/command/Command.java
+++ src/main/java/dev/amirgol/springcqrs/core/command/Command.java
@@ -4,8 +4,13 @@ package dev.amirgol.springcqrs.core.command;
  * Marker interface for commands.
  * Created by: Amir-Golmoradi
  * Created on: 2025-06-14 18:27:03
+ <br/>
+ * Marker interface for commands.
+ * Commands represent an intent to change the state of the system.
+ * They are typically immutable data-only objects.
+ *
+ * @param <R> The type of result returned by the CommandHandler after processing this command.
  */
-
 public interface Command<R> {
 
 }
