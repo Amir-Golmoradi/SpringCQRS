0cfb3907a19f41dd539345997eb726dc7ec885e6
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-16 21:26:29 +0330

    refactor(CustomerLoggedIn): Convert to record for simplified data representation

--- src/main/java/dev/amirgol/springcqrs/domain/event/CustomerLoggedIn.java
+++ src/main/java/dev/amirgol/springcqrs/domain/event/CustomerLoggedIn.java
@@ -1,25 +1,12 @@
 package dev.amirgol.springcqrs.domain.event;
 
 import dev.amirgol.springcqrs.domain.vo.CustomerId;
+import dev.amirgol.springcqrs.domain.vo.Email;
+import dev.amirgol.springcqrs.domain.vo.FullName;
+import dev.amirgol.springcqrs.domain.vo.Password;
 
 import java.time.Instant;
 
-public final class CustomerLoggedIn implements DomainEvent {
-    private final CustomerId aggregateId;
-    private final Instant occurredOn;
-
-    public CustomerLoggedIn(CustomerId aggregateId, Instant occurredOn) {
-        this.aggregateId = aggregateId;
-        this.occurredOn = occurredOn;
-    }
-
-    @Override
-    public CustomerId getAggregateId() {
-        return aggregateId;
-    }
-
-    @Override
-    public Instant getOccurredOn() {
-        return occurredOn;
-    }
+public record CustomerLoggedIn(CustomerId aggregateId, Instant occurredOn) implements DomainEvent {
 }
+
