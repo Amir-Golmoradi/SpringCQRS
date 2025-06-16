9fa7ad980fa531206c35e44048f98bc0d105665b
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-17 01:28:42 +0330

    docs(Query): Improve documentation for Query interface and clarify its purpose

--- src/main/java/dev/amirgol/springcqrs/core/query/Query.java
+++ src/main/java/dev/amirgol/springcqrs/core/query/Query.java
@@ -2,8 +2,15 @@ package dev.amirgol.springcqrs.core.query;
 
 /**
  * Marker interface for queries.
- * Created by: Amir-Golmoradi
+ * Created by: <br /> Amir-Golmoradi
  * Created on: 2025-06-14 18:27:03
+ * <br />
+ * <br />
+ * Marker interface for queries.
+ * Queries represent a request for data and do not change the state of the system.
+ * They are typically immutable data-only objects.
+ *
+ * @param <R> The type of result returned by the QueryHandler after processing this query.
  */
 public interface Query<R> {
 }
