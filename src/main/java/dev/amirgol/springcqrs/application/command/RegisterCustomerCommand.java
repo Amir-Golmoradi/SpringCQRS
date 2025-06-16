66af3f4f88e4df231886981da1d107e40d9295f7
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-17 01:32:41 +0330

    add(RegisterCustomerCommand): implement command for customer registration

---
+++ src/main/java/dev/amirgol/springcqrs/application/command/RegisterCustomerCommand.java
@@ -0,0 +1,20 @@
+package dev.amirgol.springcqrs.application.command;
+
+import dev.amirgol.springcqrs.core.command.Command;
+import dev.amirgol.springcqrs.domain.vo.CustomerId;
+import dev.amirgol.springcqrs.domain.vo.Email;
+import dev.amirgol.springcqrs.domain.vo.FullName;
+import dev.amirgol.springcqrs.domain.vo.Password;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterCustomerCommand(
+        @NotNull
+        @jakarta.validation.constraints.Email
+        Email email,
+        @NotNull
+        FullName fullName,
+        @NotNull
+        Password password
+) implements Command<CustomerId> {
+    // Command that returns UUID of created customer
+}
