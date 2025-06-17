9811b8869870ae8954d50d23dd7d990611be38a6
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-18 02:07:06 +0330

    add(UpdateCustomerCommand): enhance command with validation annotations and implement Command interface

--- src/main/java/dev/amirgol/springcqrs/application/command/UpdateCustomerCommand.java
+++ src/main/java/dev/amirgol/springcqrs/application/command/UpdateCustomerCommand.java
@@ -1,12 +1,22 @@
 package dev.amirgol.springcqrs.application.command;
 
+import dev.amirgol.springcqrs.core.command.Command;
+import dev.amirgol.springcqrs.domain.vo.CustomerId;
 import dev.amirgol.springcqrs.domain.vo.Email;
 import dev.amirgol.springcqrs.domain.vo.FullName;
 import dev.amirgol.springcqrs.domain.vo.Password;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateCustomerCommand(
+        @NotNull
+        CustomerId id,
+        @NotNull
+        @jakarta.validation.constraints.Email
         Email email,
+        @NotNull
         FullName fullName,
+        @NotNull
         Password password
-) {
+) implements Command<CustomerId> {
+    // Command that returns UUID of created customer
 }
