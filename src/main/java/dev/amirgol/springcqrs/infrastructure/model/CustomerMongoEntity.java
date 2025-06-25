ec676ee0d7f988959eecc7ae3df9142f44d5ce16
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-06-25 13:23:52 +0330

    Adds CustomerMongoEntity to support MongoDB storage
    Introduces a new `CustomerMongoEntity` class annotated for MongoDB persistence. This entity includes user-related fields such as email, full name, password, and timestamps for registration and login. Implements `UserDetails` for Spring Security integration, providing authentication and authorization capabilities.
    
    Facilitates user management and MongoDB integration in the application.

---
+++ src/main/java/dev/amirgol/springcqrs/infrastructure/model/CustomerMongoEntity.java
@@ -0,0 +1,61 @@
+package dev.amirgol.springcqrs.infrastructure.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "customers")
+public class CustomerMongoEntity implements UserDetails {
+    @Id
+    private UUID id;
+    @Indexed(unique = true)
+    private String email;
+    private String fullName;
+    private String password;
+    private Instant registrationDate;
+    private Instant lastLoginDate;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
